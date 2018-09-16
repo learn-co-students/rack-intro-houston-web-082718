c51889be24b39b33d9c7db17ec1351a2b2dc1b5f
Naeblis <johnmlowery@gmail.com>
2018-09-16 09:31:57 -0500

    Done.

--- application.rb
+++ application.rb
@@ -1,8 +1,8 @@
-class MyNewApp
+class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World! My name is Naeblis."
+    resp.write "Hello, World! I'm Nae'blis..... Hello, my name is..... <= They tell me to say this shit"
     resp.finish
   end
 
--- config.ru
+++ config.ru
@@ -1,3 +1,3 @@
 require_relative "./application.rb"
 
-run MyNewApp.new
+run Application.new
