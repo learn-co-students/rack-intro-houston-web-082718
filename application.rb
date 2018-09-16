9a22993b27880865338b02f368e8dcfb3c186b13
Naeblis <johnmlowery@gmail.com>
2018-09-16 09:30:04 -0500

    Done.

--- application.rb
+++ application.rb
@@ -1,8 +1,8 @@
-class Application
+class MyNewApp
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, World! My name is Naeblis."
     resp.finish
   end
 
